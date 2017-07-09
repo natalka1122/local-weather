c53f9309946237a8a32c7897b24f2a269c759c71
natalka1122 <natalka1122@yandex.ru>
2017-07-09 15:05:31 +0300

    https location

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        $("#location").html("Geolocation is not supported by this browser");
     }
 }
 function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
+    $("#location").html("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
 }
 
 $( document ).ready(function() {
