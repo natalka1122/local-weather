9bcd367b8a6cdf79b019c688d540e85a0627ca37
natalka1122 <natalka1122@yandex.ru>
2017-07-15 18:00:28 +0300

    foo

--- script.js
+++ script.js
@@ -1,18 +1,59 @@
+const mainDiv="#main";
+const btnUnits="#btnUnits";
+const selLanguage="#btnLanguage"
+const proxyCors="https://cors-anywhere.herokuapp.com/"
+var _0x203e=["\x38\x38\x64\x37\x39\x38\x64\x31\x32\x30\x64\x64\x36\x30\x30\x66\x38\x63\x38\x64\x34\x33\x34\x33\x35\x33\x63\x34\x33\x36\x36\x64"];const key=_0x203e[0];
+var units="si";
+var language="en";
+var latitude;
+var longitude;
+
 function getLocation() {
+    function error(err) {
+        $(mainDiv).html(`ERROR(${err.code}): ${err.message}`);
+    };
+    function success(position) {
+        latitude=position.coords.latitude;
+        longitude=position.coords.longitude;
+    }
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(success,error);
     } else {
-        $("#location").html("Geolocation is not supported by this browser");
+        $(mainDiv).html("Geolocation is not supported by this browser");
     }
 }
 function showPosition(position) {
-    $("#location").html("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
+    $(mainDiv).html("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude+" key=");
+    var jsonQuery=proxyCors+"https://api.darksky.net/forecast/"+key+"/"+position.coords.latitude+","+position.coords.longitude+"?units=si";
+    $(mainDiv).html(jsonQuery);
+    $.getJSON(jsonQuery, function(json) {
+
+        var html = json.currently.summary+"\n"+json.currently.temperature;
+        $(mainDiv).html(html);
+    });
 }
 
+function showWeather(){
+    var jsonQuery=proxyCors+"https://api.darksky.net/forecast/"+key+"/"+latitude+","+longitude+"?units="+units+"&lang="+language;
+    $(mainDiv).html(jsonQuery);
+    $.getJSON(jsonQuery, function(json) {
+
+        var html = json.currently.summary+"\n"+json.currently.temperature;
+        $(mainDiv).html(html);
+    });
+}
 $( document ).ready(function() {
     var secured=location.protocol;
-    $("#location").html("Detecting location")
+    $(mainDiv).html("Detecting location")
     if (secured!="http"){
         getLocation();
     }
+    showWeather();
+
+    $(btnUnits).click(function() {
+        alert( "Handler for btnUnits.click() called." );
+    });
+    $(selLanguage).click(function() {
+        alert( "Handler for selLanguage.click() called." );
+    });    
 });
